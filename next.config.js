63751aae2f6bad79d89cedf8ca4e542330e19cb5
Your Name <you@example.com>
2025-05-17 01:53:33 +0900

    設定ファイルを整理・簡素化し、不要なjsonファイルを削除

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   }
 }
 
